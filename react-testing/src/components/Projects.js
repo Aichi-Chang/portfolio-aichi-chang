3fc08f3d265c0542850ba4f1935f112afdf056df
Aichi Chang <phoebexxxc@gmail.com>
2020-08-07 16:12:59 +0100

    fixed links

--- react-testing/src/components/Projects.js
+++ react-testing/src/components/Projects.js
@@ -30,24 +30,24 @@ export default function Projects({ viewProjects }) {
         <img src={workbench} alt='workbench' />
         <h3>Workbench</h3>
         <p>Workbench is a schedule management app aimed at designers or students managing their on-going projects. Different users will have a clean board to themselves to start with and they will be able to add and update notes or to-do lists. The platform enables users to track their work flow online.</p>
-        <a className='button mr3' href='https://github.com/Aichi-Chang/SEI-Project-4'><p className='hover-black blue pa0 ma0'>View Site</p></a>
-        <a className='button mr3' href='https://project-4-workbench.herokuapp.com/'><p className='hover-black blue pa0 ma0'>Github</p></a>
+        <a className='button mr3' href='https://project-4-workbench.herokuapp.com/'><p className='hover-black blue pa0 ma0'>View Site</p></a>
+        <a className='button mr3' href='https://github.com/Aichi-Chang/SEI-Project-4'><p className='hover-black blue pa0 ma0'>Github</p></a>
       </div>
 
       <div className='project ma4 bg-light-gray pa4'>
         <img src={theVault} alt='the vault' />
         <h3>The Vault</h3>
         <p>The vault is an online lifestyle magazine with four different themes: second-hand clothing swap, popular locations, hot topics, and editor-selected music album, films, and book recommendations. Users will be able to register to the site. On different pages, logged in users can leave or delete comments under the articles.</p>
-        <a className='button mr3' target='_blank' href='https://github.com/Aichi-Chang/SEI-Project-3-v2'><p className='hover-black blue pa0 ma0'>View Site</p></a>
-        <a className='button mr3' target='_blank' href='https://project-3-2point0.herokuapp.com/'><p className='hover-black blue pa0 ma0'>Github</p></a>
+        <a className='button mr3' target='_blank' href='https://project-3-2point0.herokuapp.com/'><p className='hover-black blue pa0 ma0'>View Site</p></a>
+        <a className='button mr3' target='_blank' href='https://github.com/Aichi-Chang/SEI-Project-3-v2'><p className='hover-black blue pa0 ma0'>Github</p></a>
       </div>
 
       <div className='project ma4 bg-light-gray pa4'>
         <img src={aPlusL} alt='a plus l' />
         <h3>A Plus L</h3>
         <p>A + L is the initial of the team members for this project. It also stands for art and life. This is a platform where users can search for all kinds of exhibitions or lifestyle event info. They will be able to check the event location or get redirected to the actual event page from the map. The external API used for this project is from The LIST.</p>
-        <a className='button mr3' target='_blank' href='https://github.com/Aichi-Chang/SEI-Project-2'><p className='hover-black blue pa0 ma0'>View Site</p></a>
-        <a className='button mr3' target='_blank' href='https://aichi-chang.github.io/SEI-Project-2/'><p className='hover-black blue pa0 ma0'>Github</p></a>
+        <a className='button mr3' target='_blank' href='https://aichi-chang.github.io/SEI-Project-2/'><p className='hover-black blue pa0 ma0'>View Site</p></a>
+        <a className='button mr3' target='_blank' href='https://github.com/Aichi-Chang/SEI-Project-2'><p className='hover-black blue pa0 ma0'>Github</p></a>
       </div>
 
       <div className='project ma4 bg-light-gray pa4'>
